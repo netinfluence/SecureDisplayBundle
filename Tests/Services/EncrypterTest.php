a510c2ea7f951c237b528eb4d321fe986d24800a
Michael <michael.barbey@gmail.com>
2015-04-27 15:50:11 +0200

    Added PHPUnit tests

---
+++ Tests/Services/EncrypterTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Netinfluence\SecureDisplayBundle\Tests\Services;
+
+use Netinfluence\SecureDisplayBundle\Services\Encrypter;
+
+class EncrypterTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * 
+	 * @var string
+	 */
+	private $secretKey = "i am a secret key";
+
+	/**
+	 * 
+	 * @var Symfony\Component\HttpFoundation\Session\Session
+	 */
+	private $session;
+
+	/**
+	 * 
+	 * @var string
+	 */
+	private $message = "i am a message, encrypt me";
+
+	/**
+	 * (non-PHPdoc)
+	 * @see PHPUnit_Framework_TestCase::setUp()
+	 */
+	public function setUp()
+	{
+		// Don't use the real session to store the IV
+		$this->session = \Phake::mock('Symfony\Component\HttpFoundation\Session\Session');
+
+		// Return a fake IV when asked
+		\Phake::when($this->session)->get(Encrypter::SESSION_IV_KEY)->thenReturn('abcdefghijklmnop');
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see PHPUnit_Framework_TestCase::tearDown()
+	 */
+	public function tearDown()
+	{
+		$this->sessionEmpty = null;
+		$this->sessionExists = null;
+	}
+
+	/**
+	 * Ensure that the data are encrypted
+	 */
+	public function testEncryption()
+	{
+		$encrypter = new Encrypter($this->session, $this->secretKey);
+		$hash = $encrypter->encrypt($this->message);
+		$this->assertNotEquals($this->message, $hash);
+	}
+
+	/**
+	 * Ensure that the encryption-decryption process work correctly
+	 */
+	public function testDecryption()
+	{
+		$encrypter = new Encrypter($this->session, $this->secretKey);
+		$hash = $encrypter->encrypt($this->message);
+		$message = $encrypter->decrypt($hash);
+		$this->assertEquals($this->message, $message);
+	}
+}
